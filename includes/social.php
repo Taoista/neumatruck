40895ace038efc2537b40fbba7a00c1cd1fb3822
taoista <taoista-@hotmail.com>
2021-08-16 14:54:47 -0400

    actulizacion neumatruck para optimizar modelos de rrss

--- includes/social.php
+++ includes/social.php
@@ -1,10 +1,36 @@
-<div class="icon-bar text-center">
-	<a target="_blank" style="background-color:#3b5998;" href="https://www.facebook.com/neumatruck.cl/" class="facebook"><i class="fa fa-facebook"></i></a>
-	<a target="_blank" style="background-color:blue;" href="tel:+56946649909" class="google"><i class="fa fa-phone"></i></a>
-	<a target="_blank" style="background:linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d);" href="https://www.instagram.com/neumatruck.cl/" class="youtube"><i class="fa fa-instagram"></i></a>
-	<a target="_blank" style="background-color:#4FCE5D;" href="https://api.whatsapp.com/send?phone=+56981833333&amp;text=Estoy%20interesado%20en%20sus%20productos"class="twitter"><i class="fa fa-whatsapp"></i></a>
-	<!-- <a href="mailto:contacto@neumatruck.cl" class="linkedin"><i class="fa fa-envelope-o"></i></a> -->
-</div>
-<noscript>
-<img height="1" width="1" src="https://www.facebook.com/tr?id=543022966316431&ev=PageView&noscript=1"/>
-</noscript> 
+<?php
+include_once("funciones/conx.php");
+
+
+$datos		= $mysqli->query("SELECT data FROM rrss WHERE id = 1 ");
+$row		= $datos->fetch_assoc();
+$facebook 	= $row['data'];
+
+$datos		= $mysqli->query("SELECT data FROM rrss WHERE id = 2 ");
+$row		= $datos->fetch_assoc();
+$phone 		= $row['data'];
+
+$datos		= $mysqli->query("SELECT data FROM rrss WHERE id = 3 ");
+$row		= $datos->fetch_assoc();
+$instagram 	= $row['data'];
+
+$datos		= $mysqli->query("SELECT data FROM rrss WHERE id = 4 ");
+$row		= $datos->fetch_assoc();
+$whatsapp 	= $row['data'];
+
+$mysqli->close();
+
+
+
+?>
+<div class="icon-bar text-center">
+	<a target="_blank" style="background-color:#3b5998;" href="<?php echo 'https://www.facebook.com/'.$facebook.'/'?>" class="facebook"><i class="fa fa-facebook"></i></a>
+	<a target="_blank" style="background-color:blue;" href="<?php echo 'tel:'.$phone; ?>" class="google"><i class="fa fa-phone"></i></a>
+	<a target="_blank" style="background:linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d);" href=<?php echo 'https://www.facebook.com/'.$instagram.'/'?>" class="youtube"><i class="fa fa-instagram"></i></a>
+	<a target="_blank" style="background-color:#4FCE5D;" href="https://api.whatsapp.com/send?phone=<?php echo $whatsapp; ?>&amp;text=Estoy%20interesado%20en%20sus%20productos"class="twitter"><i class="fa fa-whatsapp"></i></a>
+	<!-- <a href="mailto:contacto@neumatruck.cl" class="linkedin"><i class="fa fa-envelope-o"></i></a> -->
+</div>
+<noscript>
+	<img height="1" width="1" src="https://www.facebook.com/tr?id=543022966316431&ev=PageView&noscript=1"/>
+</noscript> 
+
