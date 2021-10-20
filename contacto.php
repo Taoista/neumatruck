86264cc54bd9fa3c376f264a5a13908ea9b4882c
taoista <taoista-@hotmail.com>
2021-10-20 18:19:42 -0300

    termino del cambio de contacto

--- contacto.php
+++ contacto.php
@@ -111,7 +111,7 @@ $pagina 			= title_web($url);
 
 				<div class="col-md-7">
 
-				<form action="contacto-envia.php" class="contact-form" enctype="application/x-www-form-urlencoded" method="post">
+				<form action="#" class="contact-form" enctype="application/x-www-form-urlencoded" method="post">
 
 
 
@@ -124,91 +124,104 @@ $pagina 			= title_web($url);
 							<h3 class="title">Datos</h3>
 
 						</div>
-
-
-
 						<div class="form-group">
-
-							<input class="input" type="text" name="name" placeholder="Nombre"required>
-
+							<input id="name" class="input" type="text" name="name" placeholder="Nombre"required>
 						</div>
-
-
-
 						<div class="form-group">
-
-							<input class="input" type="email" name="email" placeholder="Email" required>
-
+							<input id="email" class="input" type="email" name="email" placeholder="Email" required>
 						</div>
-
-
-
 						<div class="form-group">
-
-							<input class="input" type="text" name="subject" placeholder="Asunto" maxlength="100" required>
-
+							<input id="phone" class="input" type="phone" name="subject" placeholder="Telefono" maxlength="100" required>
 						</div>
-
-
-
 						<div class="form-group">
-
-							<textarea class="input" name="message" placeholder="Mensaje"></textarea>
-
+							<input id="asunto" class="input" type="text" name="subject" placeholder="Asunto" maxlength="100" required>
+						</div>
+						<div class="form-group">
+							<textarea id="text-contac" class="input" name="message" placeholder="Mensaje"></textarea>
 							<input type="hidden" name="enviado" value="enviado">
-
 						</div>
-
-
-
 					</div>
-
-
-
-
-
-					<button class="primary-btn order-submit" type="submit" >Enviar Consulta </button>
+					<button onclick="send_contact_email(event)" class="primary-btn order-submit" type="submit" >Enviar Consulta </button>
 
 				</form>
 
 				</div>
 
-
-
 				<!-- Order Details -->
-
 				<div class="col-md-5">
-
 					<div class="section-title text-center">
-
 						<img src="img/img_contacto.jpg" class="img-responsive">
-
 					</div>
-
 				</div>
-
 				<!-- /Order Details -->
-
 			</div>
-
 			<!-- /row -->
-
 		</div>
-
 		<!-- /container -->
-
 	</div>
-
 	<!-- /SECTION -->
 
-
-
 	<?php include('includes/footer.php'); ?>
-
-
-
 </body>
 
+<script>
+
+	function send_contact_email(e){
+		e.preventDefault();
+		let name = document.getElementById("name").value;
+		let email = document.getElementById("email").value;
+		let asunto = document.getElementById("asunto").value;
+		let text = document.getElementById("text-contac").value;
+		let phone = document.getElementById("phone").value;
+		let correccion = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+		if(name == "" || email == "" || asunto == ""){
+			Swal.fire('Error','Debe llenar todos lo campos','error');
+		}else if(!correccion.exec(email)){
+			Swal.fire('error','El email es invalido','error');
+		}else{
+			const send_contact = new Promise((resolve, rejected) =>{
+				let parametros = {"name": name, "email" : email, "asunto" : asunto, "text" : text, "phone" : phone};
+				$.ajax({
+                data: parametros,
+                type: "POST",
+                url:  "./funciones/send_contact.php", 
+                beforeSend:function(){
+                    Swal.fire({
+                        html:'<div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>',
+                        title: 'Enviando..',
+                        showCloseButton: false,
+                        showCancelButton: false,
+                        focusConfirm: false,
+                        showConfirmButton:false,
+                    })
+                    $(".swal2-modal").css('background-color', 'rgba(0, 0, 0, 0.0)'); 
+                    $(".swal2-title").css("color","white"); 
+                },
+                success:function(response){
+                    resolve(response);
+                }
+            });
+			});
+			
+			send_contact.then(res=>{
+				swal.close();
+				if(res == "enviado"){
+					Swal.fire('Email','Email enviado correctamente','success');
+					document.getElementById("name").value = "";
+					document.getElementById("email").value = "";
+					document.getElementById("asunto").value = "";
+					document.getElementById("text-contac").value = "";
+				}else{
+					Swal.fire('Error','Ocurrio un error intente mas tarde','error');
+				}
+			})
+		
+		}
+
+
+	}
+
+</script>
 
 
 </html>
---
+++ funciones/contacto/email.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
+<head>
+<title></title>
+<meta charset="utf-8"/>
+<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+<!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+<!--[if !mso]><!-->
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css"/>
+<link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" type="text/css"/>
+<!--<![endif]-->
+<style>
+		* {
+			box-sizing: border-box;
+		}
+
+		body {
+			margin: 0;
+			padding: 0;
+		}
+
+		th.column {
+			padding: 0
+		}
+
+		a[x-apple-data-detectors] {
+			color: inherit !important;
+			text-decoration: inherit !important;
+		}
+
+		#MessageViewBody a {
+			color: inherit;
+			text-decoration: none;
+		}
+
+		p {
+			line-height: inherit
+		}
+
+		@media (max-width:700px) {
+			.icons-inner {
+				text-align: center;
+			}
+
+			.icons-inner td {
+				margin: 0 auto;
+			}
+
+			.row-content {
+				width: 100% !important;
+			}
+
+			.stack .column {
+				width: 100%;
+				display: block;
+			}
+		}
+	</style>
+</head>
+<body style="background-color: #000000; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+<table border="0" cellpadding="0" cellspacing="0" class="nl-container" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #000000;" width="100%">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f9f9f9;" width="680">
+<tbody>
+<tr>
+<th class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px;" width="100%">
+<table border="0" cellpadding="10" cellspacing="0" class="text_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+<tr>
+<td>
+<div style="font-family: sans-serif">
+<div style="font-size: 14px; color: #555555; line-height: 1.2; font-family: Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;">
+<p style="margin: 0; font-size: 14px;"></p>
+</div>
+</div>
+</td>
+</tr>
+</table>
+</th>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #000000;" width="100%">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #000000;" width="680">
+<tbody>
+<tr>
+<th class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px;" width="100%">
+<table border="0" cellpadding="0" cellspacing="0" class="image_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="width:100%;padding-right:0px;padding-left:0px;"> 
+<div align="center" style="line-height:10px"><img src="http://www.neumatruck.cl/img/logo.png" style="display: block; height: auto; border: 0; width: 500px; max-width: 100%;" width="100"/></div>
+</td>
+</tr>
+</table>
+</th>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-3" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f9f9f9;" width="680">
+<tbody>
+<tr>
+<th class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 0px; padding-bottom: 0px;" width="100%">
+<table border="0" cellpadding="0" cellspacing="0" class="heading_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="padding-bottom:10px;padding-left:30px;padding-right:30px;padding-top:40px;text-align:center;width:100%;">
+<h1 style="margin: 0; color: #000000; direction: ltr; font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 30px; font-weight: normal; letter-spacing: normal; line-height: 120%; text-align: center; margin-top: 0; margin-bottom: 0;"><?php echo ucfirst($name); ?></h1>
+</td>
+</tr>
+</table>
+<table border="0" cellpadding="0" cellspacing="0" class="heading_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="padding-bottom:10px;padding-left:30px;padding-right:30px;padding-top:10px;text-align:center;width:100%;">
+<h2 style="margin: 0; color: #000000; direction: ltr; font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 22px; font-weight: normal; letter-spacing: normal; line-height: 150%; text-align: center; margin-top: 0; margin-bottom: 0;"><?php echo $email; ?></h2>
+</td>
+</tr>
+</table>
+<table border="0" cellpadding="30" cellspacing="0" class="text_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+<tr>
+<td>
+<div style="font-family: sans-serif">
+<div style="font-size: 12px; color: #1d1d1b; line-height: 1.5; font-family: Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;">
+<p style="margin: 0; font-size: 14px; text-align: center; mso-line-height-alt: 27px;"><span style="font-size:18px;"><?php echo $asunto; ?></span></p>
+</div>
+</div>
+</td>
+</tr>
+</table>
+<table border="0" cellpadding="30" cellspacing="0" class="text_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+<tr>
+<td>
+<div style="font-family: sans-serif">
+<div style="font-size: 12px; color: #1d1d1b; line-height: 1.5; font-family: Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;">
+<p style="margin: 0; font-size: 14px; text-align: center; mso-line-height-alt: 27px;"><span style="font-size:18px;color:#8e8e8e;"><?php echo "msg: ".$text; ?></span></p>
+</div>
+</div>
+</td>
+</tr>
+</table>
+<table border="0" cellpadding="0" cellspacing="0" class="text_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+<tr>
+<td style="padding-bottom:5px;padding-left:30px;padding-right:30px;padding-top:15px;">
+<div style="font-family: sans-serif">
+<div style="font-size: 12px; color: #000000; line-height: 1.2; font-family: Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;">
+<p style="margin: 0; font-size: 14px; text-align: center;"><span style="font-size:20px;"><strong><?php echo 'Telefono: '.$phone; ?><span style=""></span></strong></span></p>
+</div>
+</div>
+</td>
+</tr>
+</table>
+<table border="0" cellpadding="0" cellspacing="0" class="text_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+<tr>
+<td style="padding-bottom:30px;padding-left:50px;padding-right:50px;padding-top:5px;">
+<div style="font-family: sans-serif">
+<div style="font-size: 12px; color: #000000; line-height: 1.2; font-family: Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;">
+<p style="margin: 0; font-size: 14px; text-align: center;"><span style="font-size:22px;"><strong></strong></span></p>
+</div>
+</div>
+</td>
+</tr>
+</table>
+<table border="0" cellpadding="0" cellspacing="0" class="button_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="padding-bottom:20px;text-align:center;">
+<div align="center">
+<!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="https://www.example.com" style="height:46px;width:251px;v-text-anchor:middle;" arcsize="0%" strokeweight="1.5pt" strokecolor="#000000" fillcolor="#000000"><w:anchorlock/><v:textbox inset="0px,0px,0px,0px"><center style="color:#ffffff; font-family:Arial, sans-serif; font-size:16px"><![endif]--><a href="https://www.neumatruck.cl" style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#000000;border-radius:0px;width:auto;border-top:2px solid #000000;border-right:2px solid #000000;border-bottom:2px solid #000000;border-left:2px solid #000000;padding-top:5px;padding-bottom:5px;font-family:Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;text-align:center;mso-border-alt:none;word-break:keep-all;" target="_blank"><span style="padding-left:60px;padding-right:60px;font-size:16px;display:inline-block;letter-spacing:normal;"><span style="font-size: 16px; line-height: 2; word-break: break-word; mso-line-height-alt: 32px;">NEUMATRUCK.CL</span></span></a>
+<!--[if mso]></center></v:textbox></v:roundrect><![endif]-->
+</div>
+</td>
+</tr>
+</table>
+<table border="0" cellpadding="0" cellspacing="0" class="heading_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="padding-bottom:10px;padding-left:30px;padding-right:30px;padding-top:30px;text-align:center;width:100%;">
+<h2 style="margin: 0; color: #000000; direction: ltr; font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 22px; font-weight: normal; letter-spacing: normal; line-height: 150%; text-align: center; margin-top: 0; margin-bottom: 0;">Dudas?</h2>
+</td>
+</tr>
+</table>
+<table border="0" cellpadding="0" cellspacing="0" class="divider_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="padding-bottom:20px;padding-right:10px;padding-top:20px;">
+<div align="center">
+<table border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="20%">
+<tr>
+<td class="divider_inner" style="font-size: 1px; line-height: 1px; border-top: 4px solid #1D1D1B;"><span><</span></td>
+</tr>
+</table>
+</div>
+</td>
+</tr>
+</table>
+<table border="0" cellpadding="30" cellspacing="0" class="heading_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td>
+<h3 style="margin: 0; color: #000000; direction: ltr; font-family: 'Cabin', Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 20px; font-weight: normal; letter-spacing: normal; line-height: 150%; text-align: center; margin-top: 0; margin-bottom: 0;">+56 2 2484 6064<br/></h3>
+</td>
+</tr>
+</table>
+</th>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-4" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f9f9f9;" width="680">
+<tbody>
+<tr>
+<th class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-left: 50px; padding-right: 30px;" width="50%">
+<table border="0" cellpadding="0" cellspacing="0" class="menu_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="padding-bottom:10px;padding-left:5px;padding-right:5px;padding-top:10px;text-align:left;color:#fbfbfb;font-family:inherit;font-size:14px;letter-spacing:0px;">
+<table border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="text-align:left;font-size:0px;">
+<div class="menu-links">
+<!--[if mso]>
+<table role="presentation" border="0" cellpadding="0" cellspacing="0" align="center">
+<tr>
+<td style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">
+<![endif]--><a href="https://www.example.com" style="padding-top:0px;padding-bottom:0px;padding-left:0px;padding-right:0px;display:block;color:#fbfbfb;font-family:Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;font-size:14px;text-decoration:none;letter-spacing:normal;">About us</a>
+<!--[if mso]></td></tr><tr><td style="text-align:left;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><![endif]--><a href="https://www.neumatruck.cl" style="padding-top:0px;padding-bottom:0px;padding-left:0px;padding-right:0px;display:block;color:#fbfbfb;font-family:Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;font-size:14px;text-decoration:none;letter-spacing:normal;">Returns</a>
+<!--[if mso]></td></tr><tr><td style="text-align:left;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><![endif]--><a href="https://www.neumatruck.cl" style="padding-top:0px;padding-bottom:0px;padding-left:0px;padding-right:0px;display:block;color:#fbfbfb;font-family:Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;font-size:14px;text-decoration:none;letter-spacing:normal;">Terms of Sale</a>
+<!--[if mso]></td></tr><tr><td style="text-align:left;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><![endif]--><a href="https://www.neumatruck.cl" style="padding-top:0px;padding-bottom:0px;padding-left:0px;padding-right:0px;display:block;color:#fbfbfb;font-family:Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;font-size:14px;text-decoration:none;letter-spacing:normal;">FAQs</a>
+<!--[if mso]></td></tr></table><![endif]-->
+</div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</th>
+<th class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top;" width="50%">
+<table border="0" cellpadding="0" cellspacing="0" class="image_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="padding-bottom:10px;padding-left:5px;padding-right:5px;padding-top:10px;width:100%;">
+<div align="center" style="line-height:10px"><a href="https://www.neumatruck.cl" style="outline:none" tabindex="-1" target="_blank"><img alt="Payment methods" src="https://www.neumatruck.cl/funciones/contacto/images/payments.png" style="display: block; height: auto; border: 0; width: 172px; max-width: 100%;" title="Payment methods" width="172"/></a></div>
+</td>
+</tr>
+</table>
+</th>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-5" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tbody>
+<tr>
+<td>
+<table align="center" border="0" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f9f9f9;" width="680">
+<tbody>
+<tr>
+<th class="column" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px;" width="100%">
+<table border="0" cellpadding="0" cellspacing="0" class="icons_block" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+<tr>
+<td style="padding-bottom:5px;padding-top:5px;color:#9d9d9d;font-family:inherit;font-size:15px;text-align:center;">
+<table cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+
+</table>
+</td>
+</tr>
+</table>
+</th>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table><!-- End -->
+</body>
+</html>
