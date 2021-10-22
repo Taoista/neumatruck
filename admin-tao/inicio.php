6542cfdb317c8924454eebec1d8291a49468d301
taoista <taoista-@hotmail.com>
2021-10-22 13:58:41 -0300

    actualizacion del proyecto por marcas

--- admin-tao/inicio.php
+++ admin-tao/inicio.php
@@ -1,9 +1,19 @@
 <?php
-    include '../includes/conx.php';
+    $mysql_hostname = "localhost";
+    $mysql_user = "neum45356_neumatruck";
+    $mysql_password = "7340458Tao";
+    $mysql_database = "neum45356_neumatruck";
+    $mysql_database = "neum45356_neumatruck";
+
+
+
+$base = new PDO('mysql:host='.$mysql_hostname.'; dbname='.$mysql_database, $mysql_user, $mysql_password);
+
+$base->exec("SET CHARACTER SET utf8");
 
 
     require_once 'modulos/PHPExcel/Classes/PHPExcel.php';
-    $archivo = "db.xlsx";
+    $archivo = "neumatruck.xlsx";
     $inputFileType = PHPExcel_IOFactory::identify($archivo);
     $objReader = PHPExcel_IOFactory::createReader($inputFileType);
     $objPHPExcel = $objReader->load($archivo);
@@ -18,35 +28,21 @@
     $cinco = array();   //  => v_lista
     // $v_oferta = array();
     for ($row = 1; $row <= $highestRow; $row++){
-            array_push($uno,$sheet->getCell("A".$row)->getValue());     // CODIGO 
+            array_push($uno,$sheet->getCell("A".$row)->getValue());     // CODIGO
             array_push($dos,$sheet->getCell("B".$row)->getValue());     // ESTADO
-            array_push($tres,$sheet->getCell("C".$row)->getValue());    // STOCK
-            array_push($cuatro,$sheet->getCell("D".$row)->getValue());  // V_PUBLICADO
-            array_push($cinco,$sheet->getCell("E".$row)->getValue());  // V_LISTA
+
     }
-
-
-
-    // for ($i=0; $i < count($uno) ; $i++) {
-
-    //     echo $uno[$i]." => ".$dos[$i]." => ".$tres[$i]." => ".$cuatro[$i]."<br>";
-
-    // }
-
-
-    // el valor V_PUBLICADO es el valor sin IVa EN EL CUAL SE VE EN LA WEB
 
     for ($i=0; $i < count($uno) ; $i++) {
-        $codigo = $uno[$i];         // => CODIGO
-        $estado = $dos[$i];         // => ESTADO
-        $stock = $tres[$i];         // => STOCK
-        $valor = $cuatro[$i];       // => V_PUBLICADO
-        $lista = $cinco[$i];        // => V_LISTA
-        $insertar = mysql_query("UPDATE productos SET estado = '$estado', stock ='$stock', v_lista = '$lista' ,v_publicado = '$valor'  WHERE codigo = '$codigo' AND oferta = 0") or die(mysql_error());
-        mysql_query($insertar);
+      $id = $uno[$i];
+      $key = $dos[$i];
+      $stmt = $base->prepare("UPDATE productos SET busqueda = '$key' WHERE id = '$id'");
+      $stmt->execute();
 
     }
 
+    $base = null;
+    $result->closeCursor();
 
 
 
@@ -58,4 +54,3 @@
 
 
 ?>
-
