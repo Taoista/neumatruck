a6528eccfd72f2960293bbe06b629423f98034ac
Taoista <taoista-@hotmail.com>
2022-09-27 14:04:10 -0300

    correccion error

--- funciones/conx.php
+++ funciones/conx.php
@@ -1,12 +1,13 @@
 <?php
 
 
+        $link = mysql_connect("localhost", "neum45356_neumatruck", "7340458Tao");
+    
+        mysql_select_db("neum45356_neumatruck",$link) OR DIE ("Error: No es posible establecer la conexión");
+    
+        mysql_set_charset('utf8');
 
-    $link = mysql_connect("localhost", "neum45356_neumatruck", "7340458Tao");
 
-    mysql_select_db("neum45356_neumatruck",$link) OR DIE ("Error: No es posible establecer la conexión");
-
-    mysql_set_charset('utf8');
 
 ?>
 
