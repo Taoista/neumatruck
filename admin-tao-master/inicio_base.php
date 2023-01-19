93c1a5ad0e5992853d1c48c9eb6e5f11892e3d0a
Alberto (taoista) <taoista-@hotmail.com>
2023-01-19 11:01:46 -0300

    cambio den el excel para actualizar por urgencia

--- admin-tao-master/inicio_base.php
+++ admin-tao-master/inicio_base.php
@@ -1,4 +1,7 @@
 <?php
+
+    echo "inciio del proeycto"."<br>";
+
     include '../includes/conx.php';
 
 
@@ -15,14 +18,19 @@
     $dos        = array();    //        => estado
     $tres       = array();    //        => stock
     $cuatro     = array();    //        => v_pulicado
-    $cinco      = array();    //        => base
+    $seis       = array();    //        => base
+    $siete       = array();    //        => base
     // $v_oferta = array();
+
+
     for ($row = 1; $row <= $highestRow; $row++){
             array_push($uno,$sheet->getCell("A".$row)->getValue());     // codigo 
             array_push($dos,$sheet->getCell("B".$row)->getValue());     // estado
             array_push($tres,$sheet->getCell("C".$row)->getValue());     // stock
             array_push($cuatro,$sheet->getCell("D".$row)->getValue());     // v_pulicado
             array_push($cinco,$sheet->getCell("E".$row)->getValue());     // base
+            array_push($seis,$sheet->getCell("G".$row)->getValue());     // stock
+            array_push($siete,$sheet->getCell("H".$row)->getValue());     // stado
     }
 
     // for ($i=0; $i < count($uno) ; $i++) {
@@ -30,23 +38,22 @@
     //     echo $uno[$i]." => ".$dos[$i]." => ".$tres[$i]." => ".$cuatro[$i]."<br>";
 
     // }
-
+    $p_descuento = 30;
     // el valor V_PUBLICADO es el valor sin IVa EN EL CUAL SE VE EN LA WEB
     for ($i=0; $i < count($uno) ; $i++) {
         $codigo         = $uno[$i];             // => codigo
-        $estado         = $dos[$i];             // => estado
-        $stock          = $tres[$i];            // => stock
-        $v_publicado    = $cuatro[$i];          // => v_pulicado
-        $v_base         = $cinco[$i];           // => base
+        $venta_sis      = $tres[$i];
+        $stock          = $seis[$i];
+        $estado         = $siete[$i];
+        $p_venta        = $venta_sis - round($venta_sis * "0.30");
 
-        $insertar = mysql_query("UPDATE productos SET stock = '$stock', v_lista = '$v_base', v_publicado = '$v_publicado'   WHERE codigo = '$codigo' AND oferta = 0") or die(mysql_error());
+        $insertar = mysql_query("UPDATE productos SET stock = '$stock', v_lista = '$venta_sis', v_publicado = '$p_venta' WHERE codigo = '$codigo' AND oferta = 0") or die(mysql_error());
         mysql_query($insertar);
 
-        $insertar = mysql_query("UPDATE productos SET stock = '$stock'  WHERE codigo = '$codigo'") or die(mysql_error());
-        mysql_query($insertar);
+        // $insertar = mysql_query("UPDATE productos SET stock = '$stock'  WHERE codigo = '$codigo'") or die(mysql_error());
+        // mysql_query($insertar);
 
     }
-
 
 
 
